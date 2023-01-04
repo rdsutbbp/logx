4d2f51592db071704da89ab00ac9eb9da3b05aa9
jaronnie <jaron@jaronnie.com>
2023-01-04 15:25:56 +0800

    feat(rdsutbbp): logx add set log level

--- log.go
+++ log.go
@@ -25,6 +25,34 @@ func Logger() *zap.Logger {
 	return logInstance
 }
 
+type Level string
+
+const (
+	// DebugLevel logs are typically voluminous, and are usually disabled in
+	// production.
+	DebugLevel Level = "debug"
+	// InfoLevel is the default logging priority.
+	InfoLevel Level = "info"
+	// WarnLevel logs are more important than Info, but don't need individual
+	// human review.
+	WarnLevel Level = "warn"
+	// ErrorLevel logs are high-priority. If an application is running smoothly,
+	// it shouldn't generate any error-level logs.
+	ErrorLevel Level = "error"
+	// DPanicLevel logs are particularly important errors. In development the
+	// logger panics after writing the message.
+	DPanicLevel Level = "dpanic"
+	// PanicLevel logs a message, then panics.
+	PanicLevel Level = "panic"
+	// FatalLevel logs a message, then calls os.Exit(1).
+	FatalLevel Level = "fatal"
+)
+
+func SetLogLevel(level Level) {
+	viper.Set("log.console_level", string(level))
+	logInstance = logger()
+}
+
 func logger() *zap.Logger {
 	infoWriter := lumberjack.Logger{
 		Filename:   defaultViperString("log.info_file", "log/info.log"), // 日志输出地址
